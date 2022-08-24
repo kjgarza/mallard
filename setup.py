d7f7f4ccbf0ea3d47b03c50acbada5b5c421a1d3
kjgarza <kgarza@datacite.org>
2022-08-24 13:56:55 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' Specifically, we examine the database structure and the level of standardization of the information provided in each field, to assess the usability of the data for bibliometric purposes.',
+    author='Kristian Garza',
+    license='MIT',
+)
